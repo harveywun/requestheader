afb1808d112b821afdc6d0c7a898b9054ee75f01
HWUN <harvey.wun@gmail.com>
2017-03-05 07:54:58 +0000

    initial commit

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+    var primaryLang = req.headers["accept-language"].split(",")[0];
+    
+    var parseSoftwareArr = req.headers['user-agent'].match(/\(([^\)]+)\)/);
+    
+    res.send({ipaddress: req.headers['x-forwarded-for'], language: primaryLang, software: parseSoftwareArr[1]});
+    //res.send("Length: " + parseSoftwareArr.length + "Text: " + parseSoftwareArr[1]);
 
+});
+
+/*
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
 })
+*/
+app.listen(process.env.PORT);
